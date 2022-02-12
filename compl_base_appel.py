7a57278d3a5ff3e7ca3a6eb19735ea7342d70bf5
Elouann Lucas <elouannlucas56@gmail.com>
2022-02-12 16:04:41 +0100

    Ajout de multiples mets simultanément

--- compl_base_appel.py
+++ compl_base_appel.py
@@ -10,7 +10,7 @@ met1 = index_appel.getvalue("met1")
 met2 = index_appel.getvalue("met2")
 met3 = index_appel.getvalue("met3")
 
-connexion = sqlite3.connect('base_vins.db')
+connexion = sqlite3.connect(database='base_vins.db')
 curseur = connexion.cursor()
 
 donnees_appel = (nom, pays, region)
@@ -21,15 +21,15 @@ def ajout_appel(donnees_appel):
     curseur.execute(
         """INSERT INTO Appellations(nom, pays, region) VALUES(?, ?, ?)""", donnees_appel)
 
-def ajout_met(donnees_mets):
+def ajout_met(met):
     """Ajoute les valeurs dans la table mets"""
-    curseur.executemany(
-        """INSERT INTO Mets(nom_met) VALUES(?)
-            INSERT INTO Mets(nom_met) VALUES(?)
-            INSERT INTO Mets(nom_met) VALUES(?)""", donnees_mets)
+    curseur.execute(
+        """INSERT INTO Mets(nom_met) VALUES(?)""", met)
 
 ajout_appel(donnees_appel)
-ajout_met(donnees_mets)
+
+for i in range (len(donnees_mets)):
+    ajout_met(donnees_mets[i])
 
 connexion.commit()
 curseur.close()
